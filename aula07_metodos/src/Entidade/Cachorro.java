28042a11e350c6db82c9ade6a18e9a3fc5efaff2
cauazz2 <caua.avlis13@gmail.com>
2024-09-05 11:40:03 -0300

    Delete aula07_metodos/src/Entidade directory

--- aula07_metodos/src/Entidade/Cachorro.java
+++
@@ -1,9 +0,0 @@
-package Entidade;
-
-public class Cachorro extends Animal{
-
-    public String EmitirSom() {
-        return super.EmitirSom() + " Au au ";
-    }
-
-}
--- aula07_metodos/src/Entidade/Gato.java
+++
@@ -1,8 +0,0 @@
-package Entidade;
-
-public class Gato extends Animal {
-
-        public String EmitirSom() {
-            return super.EmitirSom() + " Miauu ";
-        }
-}
